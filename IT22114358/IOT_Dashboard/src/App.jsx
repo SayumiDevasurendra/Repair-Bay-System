892d017350fa3a9726b85b9cb9a82958420a8a40
Anuja_Uthsara <anuja.uthsara.2002@gmail.com>
2026-03-14 16:34:56 +0530

    Update App.jsx

--- IT22114358/IOT_Dashboard/src/App.jsx
+++ IT22114358/IOT_Dashboard/src/App.jsx
@@ -2,12 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GasDashboard from './pages/GasDashboard';
 
+function NoiseRedirect() {
+  window.location.href = "http://localhost:8080"; // noise dashboard port
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/gas-monitoring" element={<GasDashboard />} />
+        <Route path="/noise-monitoring" element={<NoiseRedirect />} />
       </Routes>
     </BrowserRouter>
   );
